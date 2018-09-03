308b4ff55530c7d1b4d9d9cf078024c7bb52b8cb
Shex <2510859342@qq.com>
2018-09-03 13:46:26 +0800

    pro

--- gen/com/example/meloncalculator/R.java
+++ gen/com/example/meloncalculator/R.java
@@ -26,10 +26,36 @@ public final class R {
         public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int AC=0x7f080002;
+        public static final int DEL=0x7f080003;
+        public static final int Result=0x7f080000;
+        public static final int action_settings=0x7f080019;
+        public static final int add=0x7f08000f;
+        public static final int divide=0x7f080005;
+        public static final int dot=0x7f080017;
+        public static final int eight=0x7f080008;
+        public static final int equal=0x7f080018;
+        public static final int five=0x7f08000d;
+        public static final int four=0x7f08000c;
+        public static final int multiply=0x7f08000a;
+        public static final int nine=0x7f080009;
+        public static final int one=0x7f080011;
+        public static final int per=0x7f080004;
+        public static final int seven=0x7f080007;
+        public static final int six=0x7f08000e;
+        public static final int substract=0x7f080014;
+        public static final int tableRow1=0x7f080001;
+        public static final int tableRow2=0x7f080006;
+        public static final int tableRow3=0x7f08000b;
+        public static final int tableRow4=0x7f080010;
+        public static final int tableRow5=0x7f080015;
+        public static final int three=0x7f080013;
+        public static final int two=0x7f080012;
+        public static final int zero=0x7f080016;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int calculator=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
