b7648b409cba65b214b5bd2409b3bc293f6325f3
shoukailiang <shoukailiang@qq.com>
2018-09-03 13:32:54 +0800

    coversion

--- gen/com/example/meloncalculator/R.java
+++ gen/com/example/meloncalculator/R.java
@@ -26,18 +26,28 @@ public final class R {
         public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_area_conversion=0x7f080005;
+        public static final int action_calculator=0x7f080003;
+        public static final int action_length_conversion=0x7f080007;
+        public static final int action_settings=0x7f080002;
+        public static final int action_temperature_conversion=0x7f080004;
+        public static final int action_weight_conversion=0x7f080006;
+        public static final int button1=0x7f080000;
+        public static final int button2=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_area_conversion=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int area_conversion=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_area_conversion=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/example/meloncalculator/MainActivity.java
+++ src/com/example/meloncalculator/MainActivity.java
@@ -1,35 +1,72 @@
 package com.example.meloncalculator;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+public class MainActivity extends Activity implements OnClickListener {
+	public static String TAG = "skl";
+	private Button calcButton, covertButton;
 
-public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		bindWidgets();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	/**
+	 * °ó¶¨ÔªËØ
+	 */
+	private void bindWidgets() {
+		calcButton = (Button) findViewById(R.id.button1);
+		covertButton = (Button) findViewById(R.id.button2);
+		calcButton.setOnClickListener(this);
+		covertButton.setOnClickListener(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		System.out.println(v.getId());
+		switch (v.getId()) {
+		// Ìø×ª
+			case R.id.button1:
+				
+				break;
+			case R.id.button2:
+				Log.i("SKL", "121");
+				Intent myIntent = new Intent();
+				myIntent.setClass(MainActivity.this, AreaConversion.class);
+				startActivity(myIntent);
+				break;
+			default:
+				break;
+		}
+		
+	}
 }
