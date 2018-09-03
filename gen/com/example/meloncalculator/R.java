c862e1e1413a073adf32448beb7eff87e6decf3f
shoukailiang <shoukailiang@qq.com>
2018-09-03 18:43:17 +0800

    unit coversion

--- gen/com/example/meloncalculator/R.java
+++ gen/com/example/meloncalculator/R.java
@@ -8,48 +8,61 @@
 package com.example.meloncalculator;
 
 public final class R {
+    public static final class array {
+        public static final int length_unit_array=0x7f040001;
+        public static final int measure_array=0x7f040000;
+        public static final int weight_unit_array=0x7f040002;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Example customization of dimensions originally defined in res/values/dimens.xml
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int general_font=0x7f050003;
+        public static final int item_height=0x7f050002;
+        public static final int unit_font=0x7f050004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_area_conversion=0x7f080007;
-        public static final int action_calculator=0x7f080005;
-        public static final int action_length_conversion=0x7f080009;
-        public static final int action_settings=0x7f080004;
-        public static final int action_temperature_conversion=0x7f080006;
-        public static final int action_weight_conversion=0x7f080008;
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f080003;
-        public static final int spinner1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_area_conversion=0x7f09000d;
+        public static final int action_calculator=0x7f09000b;
+        public static final int action_length_conversion=0x7f09000f;
+        public static final int action_settings=0x7f09000a;
+        public static final int action_temperature_conversion=0x7f09000c;
+        public static final int action_weight_conversion=0x7f09000e;
+        public static final int button1=0x7f090008;
+        public static final int button2=0x7f090009;
+        public static final int input=0x7f090002;
+        public static final int input_edittext=0x7f090003;
+        public static final int input_unit=0x7f090004;
+        public static final int measure=0x7f090000;
+        public static final int result=0x7f090005;
+        public static final int result_text=0x7f090006;
+        public static final int result_unit=0x7f090007;
+        public static final int unit=0x7f090001;
     }
     public static final class layout {
         public static final int activity_area_conversion=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int area_conversion=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int area_conversion=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_area_conversion=0x7f050003;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_area_conversion=0x7f060003;
     }
     public static final class style {
         /** 
@@ -73,10 +86,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/meloncalculator/AreaConversion.java
+++ src/com/example/meloncalculator/AreaConversion.java
@@ -1,34 +1,179 @@
 package com.example.meloncalculator;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class AreaConversion extends Activity {
-
+public class AreaConversion extends Activity implements OnItemSelectedListener{
+	
+	private EditText inputNum = null;
+	private TextView inputUnit = null;
+	private TextView result = null;
+	private TextView resultUnit =null;
+	private Spinner measureSpinner = null;
+	private Spinner unitSpinner = null;	
+	private double rate = 1.0;
+	ArrayAdapter<CharSequence> unitAdapter =null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_area_conversion);
+		
+		// 输入数值
+		inputNum = (EditText)findViewById(R.id.input_edittext);
+		// 输入的    单位
+		inputUnit = (TextView)findViewById(R.id.input_unit);
+		 // 结果值
+		result = (TextView)findViewById(R.id.result_text);
+		// 结果的    单位
+		resultUnit = (TextView)findViewById(R.id.result_unit);
+		// 单位下拉
+		unitSpinner = (Spinner)findViewById(R.id.unit);
+		// 选择类型
+		measureSpinner = (Spinner)findViewById(R.id.measure);
+		
+		ArrayAdapter<CharSequence> measureAdapter = ArrayAdapter.createFromResource(this,
+		        R.array.measure_array, android.R.layout.simple_spinner_item);
+		measureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		measureSpinner.setAdapter(measureAdapter);	
+			
+		measureSpinner.setOnItemSelectedListener(this);
+		unitSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, 
+		            int pos, long id) {
+				// TODO Auto-generated method stub
+				CharSequence unit_covert = unitAdapter.getItem(pos);
+				if (unit_covert.equals("千米-米")) {
+//					Toast.makeText(MainActivity.this, "km-mile", Toast.LENGTH_SHORT).show();
+					inputUnit.setText("千米");
+					resultUnit.setText("米");
+					rate = 1000;
+				} else if (unit_covert.equals("米-码")) {
+					inputUnit.setText("米");
+					resultUnit.setText("码");
+					rate = 1.094;
+				} else if (unit_covert.equals("米-英尺")) {
+					inputUnit.setText("米");
+					resultUnit.setText("英尺");
+					rate = 3.281;
+				} else if (unit_covert.equals("厘米-英尺")) {
+					inputUnit.setText("厘米");
+					resultUnit.setText("英尺");
+					rate = 0.394;
+				} else if (unit_covert.equals("千克-英镑")) {
+					inputUnit.setText("千克");
+					resultUnit.setText("英镑");				
+					rate = 2.205;
+				} else if (unit_covert.equals("千克-盎司")) {
+					inputUnit.setText("千克");
+					resultUnit.setText("盎司");
+					rate = 35.274;
+				} else if (unit_covert.equals("海里-公里")) {
+					inputUnit.setText("海里");
+					resultUnit.setText("公里");
+					rate = 1.852;
+				} 
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		inputNum.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,     
+	                int count) {
+				// TODO Auto-generated method stub
+				String input_str = inputNum.getText().toString();
+				if (input_str !=null && !input_str.equals("")) {
+					result.setText(""+Double.parseDouble(input_str)*rate);
+				}else {
+					result.setText("0.0");
+				}
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.area_conversion, menu);
-		return true;
-	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public void onItemSelected(AdapterView<?> parent, View view, 
+            int pos, long id) {
+		// TODO Auto-generated method stub
+		switch (pos) {
+		case 0:{
+				unitAdapter = ArrayAdapter.createFromResource(this,
+		        R.array.length_unit_array, android.R.layout.simple_spinner_item);
+				unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				unitSpinner.setAdapter(unitAdapter);		
+			}
+			
+			break;
+		case 1:{
+				unitAdapter = ArrayAdapter.createFromResource(this,
+						R.array.weight_unit_array, android.R.layout.simple_spinner_item);
+				unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				unitSpinner.setAdapter(unitAdapter);
+			}
+			
+			break;
+		default:
+			break;
 		}
-		return super.onOptionsItemSelected(item);
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+	}
+
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		Log.d("areaActivity", "dispatchKeyEvent->"+event.getKeyCode());
+		// TODO Auto-generated method stub
+		if (event.getKeyCode()==KeyEvent.KEYCODE_MENU&&!inputNum.getText().toString().equals("")) {
+			long now = SystemClock.uptimeMillis(); 
+			KeyEvent newKey = new KeyEvent(now, now, event.getAction(), KeyEvent.KEYCODE_NUMPAD_DOT, 0);
+			return super.dispatchKeyEvent(newKey);
+		}
+		return super.dispatchKeyEvent(event);
+	}
+
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		Log.d("areaActivity", "onKeyDown->"+event.getKeyCode());
+		// TODO Auto-generated method stub
+		return super.onKeyDown(keyCode, event);
 	}
 }
